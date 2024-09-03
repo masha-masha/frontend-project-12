97f95f4b7fab82f1dc4104a07b4c6d8515e14402
Maria Bazhenova <yankeevsdixie@gmail.com>
2024-09-04 00:39:35 +0300

    added formik, route, yup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HomePage } from './components/HomePage';
+import { LoginPage } from './components/LoginPage';
+import { NotFoundPage } from './components/NotFoundPage';
+
+     
+
+
+const App = ()  => {
+  return (
+    <BrowserRouter>
+      <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;

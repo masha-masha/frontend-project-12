cb0dfa99949bad6c8470bdeb7c6bf3ace73a5cb2
Maria Bazhenova <yankeevsdixie@gmail.com>
2024-11-07 22:42:24 +0300

    renamed submitbutton in rename modal

--- frontend/src/locales/ru.js
+++ frontend/src/locales/ru.js
@@ -74,7 +74,7 @@ const ru = {
       rename: {
         title: 'Переименовать канал',
         cancelButton: 'Отменить',
-        submitButton: 'Отправить',
+        submitButton: 'Переименовать',
       },
     },
     toastify: {
